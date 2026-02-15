6d66e9caf849604ff7dd33a664a6530f0fcdec39
Natanael Batista de Fontes Nobre <natanaelbr.nobre@gmail.com>
2026-02-15 13:04:44 -0300

    Resolução de todos os excercícios de Condicionais, do basico ao avançado

--- exercicios.js
+++ exercicios.js
@@ -98,17 +98,83 @@
 // }else{
 //     alert(`O terceiro número ${numero3} é maior que ${numero1} e ${numero2}`)
 // }
+
 // ================================================== Ex03 10% OFF acima de 100
+// let numero1 = Number(prompt("Digite o valor do primeiro produto"));
+// let numero2 = Number(prompt("Digite o valor do segundo produto"));
+// let numero3 = Number(prompt("Digite o valor do terceiro produto"));
+// let totalprod = (numero1 + numero2 + numero3);
+// let total;
+ 
+// if (totalprod >= 100) {
+//     total = totalprod - (totalprod * (10 / 100));
+//     alert(`Sua compra de ${totalprod} teve um desconto de 10% e passou a valer: ${total}`)
+// } else {
+//     alert(`Sua compra ficou ${total}`)
+// }
 
-let numero1 = Number(prompt("Digite o valor do primeiro produto"))
-let numero2 = Number(prompt("Digite o valor do segundo produto"))
-let numero3 = Number(prompt("Digite o valor do terceiro produto"))
-total = numero1 + numero2 + numero3
+// ================================================== Ex04 Sistema de login simples
+// let usuario = String(prompt("Digite seu nome de usuário")).toLowerCase();
+// let senha = prompt("Digite sua senha")
 
-if (total >= 100) {
-    total = total - (10 / 100)
-    alert = ("Sua compra teve um desconto de 10% e passou a valer: ", total)
-}else{
-    alert(`Sua compra ficou ${total}`)
-}
+// if (usuario === "admin" && senha === "1234") {
+//     alert("Login bem-sucedido! Bem-vindo, admin.");
+// } else {
+//     alert("Login falhou! Nome de usuário ou senha incorretos.");
+// }
 
+
+
+// ================================================== Ex01 Classificação de triângulos;
+//Considere Triângulo equilátero (3 lados iguais), Triângulo isósceles (2 lados iguais) e Triângulo escaleno (3 lados diferentes).
+// let lado1 = Number(prompt("Digite o valor do primeiro lado do triângulo"))
+// let lado2 = Number(prompt("Digite o valor do segundo lado do triângulo"))
+// let lado3 = Number(prompt("Digite o valor do terceiro lado do triângulo"))
+
+// if (lado1 === lado2 && lado2 === lado3) {
+//     alert("O triângulo é equilátero");
+// } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+//     alert("O triângulo é isósceles");
+// } else {
+//     alert("O triângulo é escaleno");
+// }
+
+// ================================================== Ex02 Conversão de notas para conceitos:
+// A (90-100), B (80-89), C (70-79), D (60-69), F (<60).
+// let nota = Number(prompt("Digite a primeira nota do aluno"))
+// let nota2 = Number(prompt("Digite a segunda nota do aluno"))
+// let nota3 = Number(prompt("Digite a terceira nota do aluno"))
+// let media = (nota + nota2 + nota3) / 3
+// if (media >= 90 && media <= 100) {
+//     alert(`A média do aluno é ${media} e o conceito é A`);
+// } else if (media >= 80 && media < 90) {
+//     alert(`A média do aluno é ${media} e o conceito é B`);
+// }   else if (media >= 70 && media < 80) {
+//     alert(`A média do aluno é ${media} e o conceito é C`);
+// } else if (media >= 60 && media < 70) {
+//     alert(`A média do aluno é ${media} e o conceito é D`);
+// } else {
+//     alert(`A média do aluno é ${media} e o conceito é F`);
+// }
+// ================================================== Ex03 Cálculo de IMC
+// let peso = Number(prompt("Digite seu peso em kg"))
+// let altura = Number(prompt("Digite sua altura em metros"))
+// let imc = peso / (altura * altura)
+// alert (`Seu IMC é ${imc.toFixed(2)}`)
+
+// if (imc < 18.5) {
+//     alert(`Seu IMC é ${imc.toFixed(2)} e você está abaixo do peso.`);
+// } else if (imc >= 18.5 && imc <24.99){
+//     alert(`Seu IMC é ${imc.toFixed(2)} e você está com peso normal.`);
+// }else if (imc >= 25 && imc < 29.99){
+//     alert(`Seu IMC é ${imc.toFixed(2)} e você está com sobrepeso.`);
+// }else{
+//     alert(`Seu IMC é ${imc.toFixed(2)} e você está com obesidade.`);
+// }
+// ================================================== Ex04 Verificação de ano bissexto
+// let ano = Number(prompt("Digite um ano para verificar se é bissexto: "))
+// if ((ano % 4 === 0 && ano % 100) || (ano % 400 === 0))  {
+//     alert(`O ano ${ano} é bissexto.`);
+// } else {    
+//     alert(`O ano ${ano} não é bissexto.`);
+// }
